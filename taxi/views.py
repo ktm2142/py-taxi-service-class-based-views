b0819c53d0fa0c904352af61e0ae113477f9fd0f
Volodymyr Oliinyk <ktm2142@gmail.com>
2026-09-10 17:19:43 +0000

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -29,7 +29,9 @@ class CarListView(generic.ListView):
 
 class CarDetailView(generic.DetailView):
     model = Car
-    queryset = Car.objects.select_related("manufacturer").prefetch_related("drivers")
+    queryset = (Car.objects
+                .select_related("manufacturer")
+                .prefetch_related("drivers"))
 
 
 class DriverListView(generic.ListView):
