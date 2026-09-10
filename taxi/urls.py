38f657e9ee91caf837ecacb3a20367c3f3729e8c
Volodymyr Oliinyk <ktm2142@gmail.com>
2026-09-10 17:09:43 +0000

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     ),
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
-    path("driver/", DriverListView.as_view(), name="driver-list"),
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/",
         DriverDetailView.as_view(),
--- taxi/views.py
+++ taxi/views.py
@@ -24,11 +24,12 @@ class ManufacturerListView(generic.ListView):
 class CarListView(generic.ListView):
     model = Car
     paginate_by = 5
-    queryset = Car.objects.all().select_related("manufacturer")
+    queryset = Car.objects.select_related("manufacturer")
 
 
 class CarDetailView(generic.DetailView):
     model = Car
+    queryset = Car.objects.select_related("manufacturer")
 
 
 class DriverListView(generic.ListView):
@@ -38,4 +39,4 @@ class DriverListView(generic.ListView):
 
 class DriverDetailView(generic.DetailView):
     model = Driver
-    queryset = Driver.objects.all().prefetch_related("cars")
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
